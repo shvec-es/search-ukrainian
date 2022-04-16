0a8f4705dc826abb627d047e28b35a2f67eece68
shvec-es <kotova.es.1510@gmail.com>
2022-04-16 16:20:10 +0300

    first settings

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Container from './Container';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import styles from './Header.module.scss';
+import logo from '../../images/logo.jpg';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src={logo} alt="logo" />
+      </div>
+      <nav className={styles.nav}>
+        <ul className={styles.nav_list}>
+          <li className={styles.nav_item}>Головна</li>
+          <li className={styles.nav_item}>Зв'язатись з розробниками</li>
+          <li className={styles.nav_item}>Підтримати проект</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
