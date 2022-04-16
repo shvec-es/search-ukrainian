f17a9b59bf379ae6cf651453a86ce65a1d59c07e
Dima Sotskiy <sotskiydr@gmail.com>
2022-04-16 23:35:13 +0300

    created list , item components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Container from './Container';
 import Header from './Header/Header';
+import List from './Main/List/List';
 
 function App() {
   return (
     <Container>
       <Header />
+      <List />
     </Container>
   );
 }
