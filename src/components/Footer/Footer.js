8e4311d79d29c2fed96278a6b221323ece5738a0
shvec-es <kotova.es.1510@gmail.com>
2022-04-17 17:26:23 +0300

    Add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+function Footer() {
+  return (
+    <div className={styles.footer}>
+      <p className={styles.footer_text}>Copyright 2022</p>
+    </div>
+  );
+}
+
+export default Footer;
