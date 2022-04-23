93ed92dc856c892d15995a971cb163d3f11b5828
Dima Sotskiy <sotskiydr@gmail.com>
2022-04-23 15:50:53 +0300

    add request

--- src/components/ApiServices/ApiService.js
+++ src/components/ApiServices/ApiService.js
@@ -1,10 +1,22 @@
 import axios from 'axios';
 
 class ApiService {
-  async postCard(object) {
+  async postCard(object, file) {
     try{
-      const req = await axios.post('http://localhost:8989/api/posts', object)
-      console.log(req)
+      const fd = new FormData();
+      fd.append('name' , object.name)
+      fd.append('surname' , object.surname)
+      fd.append('midname' , object.midname)
+      fd.append('city' , object.city)
+      fd.append('region' , object.region)
+      fd.append('birth_year' , object.birth_year)
+      fd.append('description' , object.description)
+      fd.append('picture', file)
+      const req = await axios.post('http://localhost:8989/api/posts', fd, {
+          headers: {
+            'Content-Type': ' multipart/form-data'
+          }
+      })
     }catch (e) {
       console.log(e)
     }
