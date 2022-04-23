b0668a814718dd9456c094a9e1da63d942c39a7c
shvec-es <kotova.es.1510@gmail.com>
2022-04-23 15:55:08 +0300

    second try

--- src/components/ApiServices/ApiService.js
+++ src/components/ApiServices/ApiService.js
@@ -2,13 +2,22 @@ import axios from 'axios';
 
 class ApiService {
   async postCard(object) {
-    try{
-      const req = await axios.post('http://localhost:8989/api/posts', object)
-      console.log(req)
-    }catch (e) {
-      console.log(e)
+    try {
+      const req = await axios.post('http://localhost:8989/api/posts', object);
+      console.log(req);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async getAll(object) {
+    try {
+      const req = await axios.get('http://localhost:8989/api/posts', object);
+      console.log(req);
+    } catch (e) {
+      console.log(e);
     }
   }
 }
 
-export default new ApiService
+export default new ApiService();
