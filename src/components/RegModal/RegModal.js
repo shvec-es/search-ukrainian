1e0459e31956c76d963d8307938bae53528db1a3
Dima Sotskiy <sotskiydr@gmail.com>
2022-04-23 14:01:39 +0300

    add state at regmodal

--- src/components/RegModal/RegModal.js
+++ src/components/RegModal/RegModal.js
@@ -1,70 +1,154 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './RegModal.module.scss'
 import btnSvg from '../../images/ic_baseline-add-a-photo.svg'
+import btnSuccSvg from '../../images/success-svgrepo-com.svg'
+import ApiService from '../ApiServices/ApiService';
 
 const RegModal = () => {
-  return (
-    <div className={styles['modal-container']}>
-      <form action=''>
-        <ul className={styles['list']}>
-          <li className={styles['input-thumb']}>
-            <label className={styles['input-label']} htmlFor='id-name'>
-              Ім’я:
-            </label>
-            <input className={styles.input} id='id-name' type='text' />
-          </li>
-          <li className={styles['input-thumb']}>
-            <label className={styles['input-label']} htmlFor='id-last_name'>
-              Прізвище:
-            </label>
-            <input className={styles.input} id='id-last_name' type='text' />
-          </li>
-          <li className={styles['input-thumb']}>
-            <label className={styles['input-label']} htmlFor='id-middle_name'>
-              По батькові:
-            </label>
-            <input className={styles.input} id='id-middle_name' type='text' />
-          </li>
-          <li className={styles['input-thumb']}>
+  const [formState, setFormState] = useState({
+    name: '',
+    surname: '',
+    midname: '',
+    city: '',
+    region: '',
+    birth_year: '',
+    description: '',
+    picture: ''
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormState({ ...formState, [name]: value })
+  }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      ApiService.postCard(formState)
+    }
+
+    return (
+      <div className={styles['modal-container']}>
+        <form onSubmit={handleSubmit}>
+          <ul className={styles['list']}>
+            <li className={styles['input-thumb']}>
+              <label className={styles['input-label']} htmlFor='id-name'>
+                Ім’я:
+              </label>
+              <input name='name'
+                     className={styles.input}
+                     id='id-name'
+                     value={formState.firstName}
+                     onChange={handleChange}
+                     type='text'
+                     required
+              />
+            </li>
+            <li className={styles['input-thumb']}>
+              <label className={styles['input-label']} htmlFor='id-last_name'>
+                Прізвище:
+              </label>
+              <input name='surname'
+                     className={styles.input}
+                     id='id-last_name'
+                     onChange={handleChange}
+                     value={formState.surname}
+                     type='text'
+                     required
+              />
+            </li>
+            <li className={styles['input-thumb']}>
+              <label className={styles['input-label']} htmlFor='id-middle_name'>
+                По батькові:
+              </label>
+              <input name='midname'
+                     className={styles.input}
+                     id='id-middle_name'
+                     onChange={handleChange}
+                     value={formState.midname}
+                     type='text'
+                     required
+              />
+            </li>
+            <li className={styles['input-thumb']}>
               <label className={styles['input-label']} htmlFor='id-city'>
                 Місто:
               </label>
-              <input className={styles.input} id='id-city' type='text' />
-          </li>
-          <li className={styles['input-thumb']}>
+              <input name='city'
+                     className={styles.input}
+                     id='id-city'
+                     onChange={handleChange}
+                     value={formState.city}
+                     type='text'
+                     required
+              />
+            </li>
+            <li className={styles['input-thumb']}>
               <label className={styles['input-label']} htmlFor='id-oblast'>
                 Область:
               </label>
-              <input className={styles.input} id='id-oblast' type='text' />
-          </li>
-          <li className={styles['input-thumb']}>
+              <input name='region'
+                     className={styles.input}
+                     id='id-oblast'
+                     onChange={handleChange}
+                     value={formState.region}
+                     type='text'
+                     required
+              />
+            </li>
+            <li className={styles['input-thumb']}>
               <label className={styles['input-label']} htmlFor='id-date_of_birth'>
                 Рік народження:
               </label>
-              <input className={styles.input} id='id-date_of_birth' type='text' />
-          </li>
-        </ul>
+              <input name='birth_year'
+                     className={styles.input}
+                     id='id-date_of_birth'
+                     onChange={handleChange}
+                     value={formState.birth_year}
+                     type='date'
+                     required
+              />
+            </li>
+          </ul>
 
-        <ul className={styles['text_area-list']}>
-          <li className={styles['input-text_area']}>
-            <label className={styles['label-description']} htmlFor='id-description'>
-            Додаткова інформація:
-          </label>
-            <textarea className={styles.description} id='id-description' name='' cols='30' rows='10'>
+          <ul className={styles['text_area-list']}>
+            <li className={styles['input-text_area']}>
+              <label className={styles['label-description']} htmlFor='id-description'>
+                Додаткова інформація:
+              </label>
+              <textarea name='description'
+                        className={styles.description}
+                        id='id-description'
+                        onChange={handleChange}
+                        value={formState.description}
+                        cols='30'
+                        rows='10'
+                        required
+              >
           </textarea>
-          </li>
-          <li className={styles['input-thumb']}>
-            <input className={styles['add-picture']} type='file' />
-              {/*<img className={styles.svgBtn} src={btnSvg} alt='svg' />*/}
-              {/*Додати зображення людини*/}
+            </li>
+            <li className={styles['input-thumb']}>
+              <label htmlFor='from-add_picture' className={styles['add-picture']}>
+                <img className={styles.svgBtn} src={btnSvg} alt='svg' />
+                Додати зображення людини
+                {formState.picture && <img className={styles['svgSuccBtn']} src={btnSuccSvg} alt='svgSuccess' />}
+                {formState.picture && <p className={styles.befEl}>Фотографія успішно завантажена</p>}
+              </label>
+              <input name='picture'
+                     id='from-add_picture'
+                     type='file'
+                     onChange={handleChange}
+                     value={formState.picture}
+                     hidden
+                     accept="image/*" multiple={false}
+                     required
+              />
 
-            <button className={styles['submit-btn']} type='submit'>Залишити інформацію</button>
-          </li>
-        </ul>
+              <button className={styles['submit-btn']} type='submit'>Залишити інформацію</button>
+            </li>
+          </ul>
 
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
 };
 
-export default RegModal;
+export default RegModal
