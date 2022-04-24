fd99980174f3a584d2294804a533074e7fa101ae
shvec-es <kotova.es.1510@gmail.com>
2022-04-24 15:42:33 +0300

    fix mail styles

--- src/components/Main/Item/Item.js
+++ src/components/Main/Item/Item.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import styles from '../Item/Item.module.scss'
+import React, { useState } from 'react';
+import styles from '../Item/Item.module.scss';
 
+const Item = ({
+  itemData: {
+    _id: id,
+    picture,
+    name,
+    surname,
+    midname,
+    birth_year,
+    region,
+    city,
+    description,
+  },
+}) => {
+  const [openInfo, setOpenInfo] = useState(false);
 
-const Item = ({itemData: {id, img, name, dateOfBirth, oblast, city }}) => {
+  const toOpenAllInfo = () => {
+    setOpenInfo(prevState => !prevState);
+  };
 
   return (
-    <li key={id} className={styles.item}>
-      <img className={styles.img} src={img} alt={id} />
+    <li
+      key={id}
+      className={!openInfo ? styles.item : styles.item + ' ' + styles.open}
+      onClick={toOpenAllInfo}
+    >
+      <img
+        className={styles.picture}
+        src={`http://localhost:8989/${picture}`}
+        alt={id}
+      />
       <div className={styles['item-description']}>
-        <h3 className={styles['item-title']}>{name}</h3>
-        <p className={styles.date}>{dateOfBirth}</p>
-        <p className={styles.description}>Область: {oblast}</p>
-        <p className={styles.description}>Місто: {city}</p>
+        <h3 className={styles['item-title']}>
+          {surname} {name} {midname}
+        </h3>
+        <p className={styles.date}>{birth_year}</p>
+        <p className={styles.description}>
+          <b>Область:</b> {region}
+        </p>
+        <p className={styles.description}>
+          <b>Місто:</b> {city}
+        </p>
+        <p className={styles.details}>
+          <b>Обставини:</b> {description}
+        </p>
+        <p className={styles.tocontact}>
+          Якщо ви володієте інформацією про місце перебування цієї особи,
+          повідомте, будь ласка, сюди:
+        </p>
+        <ul className={styles.contactlist}>
+          <li className={styles.contactitem}>
+            <a className={styles.link} href="mailto:dmytro.selikhov@gmail.com">
+              <svg className={styles.icon} width="30px" height="30px">
+                <use href="../../../images/symbol-defs.svg#icon-mail"></use>
+              </svg>
+              dmytro.selikhov@gmail.com
+            </a>
+          </li>
+          <li className={styles.contactitem}>
+            <a className={styles.link} href="tel:+380506698759">
+              <svg className={styles.icon} width="24px" height="24px">
+                <use href="../../../images/symbol-defs.svg#icon-phone"></use>
+              </svg>
+              0506698759, 0673285329
+            </a>
+          </li>
+        </ul>
       </div>
     </li>
   );
--- src/components/Main/List/List.js
+++ src/components/Main/List/List.js
@@ -1,80 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import ApiService from '../../ApiServices/ApiService';
 import Item from '../Item/Item';
-import { v4 as uuidv4 } from 'uuid';
 import styles from './List.module.scss';
 
 const List = () => {
-  const [itemData, setItemData] = useState([
-    {
-      id: uuidv4(),
-      img: 'http://kartina-optom.com.ua/images/stories/virtuemart/product/50%D1%8550.jpg',
-      name: 'Колесник Валерій Валерійович',
-      dateOfBirth: '24.07.1959',
-      oblast: 'Донецька',
-      city: 'Маріуполь',
-    },
-    {
-      id: uuidv4(),
-      img: 'http://kartina-optom.com.ua/images/stories/virtuemart/product/50%D1%8550.jpg',
-      name: 'Колесник Валерій Валерійович',
-      dateOfBirth: '24.07.1959',
-      oblast: 'Донецька',
-      city: 'Маріуполь',
-    },
-    {
-      id: uuidv4(),
-      img: 'http://kartina-optom.com.ua/images/stories/virtuemart/product/50%D1%8550.jpg',
-      name: 'Колесник Валерій Валерійович',
-      dateOfBirth: '24.07.1959',
-      oblast: 'Донецька',
-      city: 'Маріуполь',
-    },
-    {
-      id: uuidv4(),
-      img: 'http://kartina-optom.com.ua/images/stories/virtuemart/product/50%D1%8550.jpg',
-      name: 'Колесник Валерій Валерійович',
-      dateOfBirth: '24.07.1959',
-      oblast: 'Донецька',
-      city: 'Маріуполь',
-    },
-    {
-      id: uuidv4(),
-      img: 'http://kartina-optom.com.ua/images/stories/virtuemart/product/50%D1%8550.jpg',
-      name: 'Колесник Валерій Валерійович',
-      dateOfBirth: '24.07.1959',
-      oblast: 'Донецька',
-      city: 'Маріуполь',
-    },
-    {
-      id: uuidv4(),
-      img: 'http://kartina-optom.com.ua/images/stories/virtuemart/product/50%D1%8550.jpg',
-      name: 'Колесник Валерій Валерійович',
-      dateOfBirth: '24.07.1959',
-      oblast: 'Донецька',
-      city: 'Маріуполь',
-    },
-    {
-      id: uuidv4(),
-      img: 'http://kartina-optom.com.ua/images/stories/virtuemart/product/50%D1%8550.jpg',
-      name: 'Колесник Валерій Валерійович',
-      dateOfBirth: '24.07.1959',
-      oblast: 'Донецька',
-      city: 'Маріуполь',
-    },
-    {
-      id: uuidv4(),
-      img: 'http://kartina-optom.com.ua/images/stories/virtuemart/product/50%D1%8550.jpg',
-      name: 'Колесник Валерій Валерійович',
-      dateOfBirth: '24.07.1959',
-      oblast: 'Донецька',
-      city: 'Маріуполь',
-    },
-  ]);
+  const [itemData, setItemData] = useState([]);
+
+  useEffect(() => {
+    ApiService.getAll().then(data => {
+      setItemData(data);
+    });
+  }, [itemData]);
 
   return (
     <ul className={styles['main-list']}>
       {itemData.map(el => {
-        return <Item key={el.id} itemData={el} />;
+        return <Item key={el._id} itemData={el} />;
       })}
     </ul>
   );
